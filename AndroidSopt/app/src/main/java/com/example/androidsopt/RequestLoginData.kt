cf2ded817ea88cc646618564d935e48b0110a5c2
kimsubeen <papajj06@naver.com>
2021-10-31 15:04:29 +0900

    +4주차세미나_retrofit

--- AndroidSopt/app/src/main/java/com/example/androidsopt/RequestLoginData.kt
+++ AndroidSopt/app/src/main/java/com/example/androidsopt/RequestLoginData.kt
@@ -1,3 +1,9 @@
 package com.example.androidsopt
 
-data class RequestLoginData()
+import com.google.gson.annotations.SerializedName
+
+data class RequestLoginData(
+    @SerializedName("email")
+    val email : String,
+    val password : String
+)
--- AndroidSopt/app/src/main/java/com/example/androidsopt/SampleService.kt
+++ AndroidSopt/app/src/main/java/com/example/androidsopt/SampleService.kt
@@ -1,4 +1,15 @@
 package com.example.androidsopt
 
-interface SampleService {
-}
+import android.telecom.Call
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+//
+//interface SampleService {
+//@Headers("Content-Type:application/json")
+//@POST("user/login")
+//fun postLogin(
+//    @Body body : RequestLoginData
+//): Call<ResponseLoginData>
+//
+//}
