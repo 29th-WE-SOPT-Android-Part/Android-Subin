5ba7fcb64fb4073706f04c50fed5694226bccfe3
kimsubeen <papajj06@naver.com>
2021-10-31 18:06:13 +0900

    [REFACTOR] 로그인,회원가입
    id,pw intent구현

--- AndroidSopt/app/src/main/java/com/example/androidsopt/SignInActivity.kt
+++ AndroidSopt/app/src/main/java/com/example/androidsopt/SignInActivity.kt
@@ -4,36 +4,77 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.androidsopt.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
-    private lateinit var binding : ActivitySignInBinding
+    private lateinit var binding: ActivitySignInBinding
+
+    //intent값 가져오기
+    private lateinit var getResultTextView: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivitySignInBinding.inflate(layoutInflater)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
 
-//        val id: String =binding.etId.text.toString()
-//        val pw=binding.etId2.text.toString()
-        val intent= Intent(this, SignUpActivity::class.java)
-        //회원가입 버튼 누르면 SignUpActivity로 이동
-        val intent2=Intent(this, HomeActivity::class.java)
-
-
-        binding.btnSign.setOnClickListener{
-            startActivity(intent)
-        }
-
-        binding.btnLogin.setOnClickListener{
-            if (binding.etId.text.toString().equals("")||binding.etId2.text.toString().equals("")) {
-                Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
-            }
-            else {
-                Toast.makeText(this, binding.etId.text.toString() + " 님 환영합니다.", Toast.LENGTH_SHORT)
-                    .show()
-                startActivity(intent2)
-            }
-        }
         setContentView(binding.root)
-    }
+        LoginBtnEvent()
+        SignUpBtnEvent()
     }
 
+    private fun LoginBtnEvent() {
+        val intentHome = Intent(this, HomeActivity::class.java)
+        binding.apply {
+            btnLogin.setOnClickListener {
+                val userId = etId.text.toString()
+                val userPW = etId2.text.toString()
+
+                if (userId.isNotEmpty() && userPW.isNotEmpty()) {
+                    startActivity(intentHome)
+                    Toast.makeText(this@SignInActivity, "$userId 님 환영합니다", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(this@SignInActivity, "로그인 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    private fun SignUpBtnEvent() {
+        val start = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult -> }
+        binding.btnSign.setOnClickListener {
+            start.launch(Intent(this, SignUpActivity::class.java))
+        }
+
+        if (intent.hasExtra("id") && intent.hasExtra("pw")) {
+            val id = intent.getStringExtra("id")
+            val pw = intent.getStringExtra("pw")
+
+            binding.etId.setText(id)
+            binding.etId2.setText(pw)
+        }
+    }
+
+}
+//val id: String =binding.etId.text.toString()
+////        val pw=binding.etId2.text.toString()
+//val intent= Intent(this, SignUpActivity::class.java)
+////회원가입 버튼 누르면 SignUpActivity로 이동
+//val intent2=Intent(this, HomeActivity::class.java)
+//
+//
+//binding.btnSign.setOnClickListener{
+//    startActivity(intent)
+//}
+//
+//binding.btnLogin.setOnClickListener{
+//    if (binding.etId.text.toString().equals("")||binding.etId2.text.toString().equals("")) {
+//        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+//    }
+//    else {
+//        Toast.makeText(this, binding.etId.text.toString() + " 님 환영합니다.", Toast.LENGTH_SHORT)
+//            .show()
+//        startActivity(intent2)
+//    }
+//}
+//setContentView(binding.root)
--- AndroidSopt/app/src/main/java/com/example/androidsopt/SignUpActivity.kt
+++ AndroidSopt/app/src/main/java/com/example/androidsopt/SignUpActivity.kt
@@ -13,18 +13,41 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding= ActivitySignUpBinding.inflate(layoutInflater)
-        binding.btnsignup.setOnClickListener{
-                if (binding.etId.text.toString().equals("")||binding.etId2.text.toString().equals("")||binding.etName.text.toString().equals("")) {
-                    Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_LONG).show()
-                }
-            else
-               finish()
-        }
 
         setContentView(binding.root)
+        btnEvent()
+    }
+    private fun btnEvent(){
+        val intent = Intent(this, SignInActivity::class.java)
+        binding.apply{
+            btnsignup.setOnClickListener{
+                val userName = etName.text.toString()
+                val userId = etId.text.toString()
+                val userPW = etId2.text.toString()
+
+                if (userName.isNotEmpty() && userId.isNotEmpty() && userPW.isNotEmpty()){
+                    intent
+                        .putExtra("id", userId)
+                        .putExtra("pw", userPW)
+                    startActivity(intent)
+                    finish()
+
+                } else {
+                    Toast.makeText(this@SignUpActivity, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
     }
 
     }
 
 
 //activity_sign_up
+//binding.btnsignup.setOnClickListener{
+//    if (binding.etId.text.toString().equals("")||binding.etId2.text.toString().equals("")||binding.etName.text.toString().equals("")) {
+//        Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_LONG).show()
+//    }
+//    else
+//        finish()
+//}
