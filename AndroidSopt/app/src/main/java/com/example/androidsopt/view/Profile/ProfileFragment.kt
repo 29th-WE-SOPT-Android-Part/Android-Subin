8377017f3da8a2141682a52bf5b289ed5cd2c5b3
kimsubeen <papajj06@naver.com>
2022-01-25 00:54:50 +0900

    [REFACTOR] 기존 코드 주석 제거

--- AndroidSopt/app/src/main/java/com/example/androidsopt/view/Profile/ProfileFragment.kt
+++ AndroidSopt/app/src/main/java/com/example/androidsopt/view/Profile/ProfileFragment.kt
@@ -14,7 +14,7 @@ import com.example.androidsopt.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private var _binding :FragmentProfileBinding? = null
+    private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,7 +22,6 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
-//
 
         initSettings()
         initTransactionEvent()
@@ -30,29 +29,30 @@ class ProfileFragment : Fragment() {
 
         return binding.root
     }
-    private fun initTransactionEvent(){
+
+    private fun initTransactionEvent() {
         val followerFragment = FollowerFragment()
         val repositoryFragment = RepositoryFragment()
 
         childFragmentManager.beginTransaction().add(R.id.container_view, followerFragment).commit()
         binding.btnFollower.isSelected = true //처음 화면 보여질 시에
 
-        binding.btnFollower.setOnClickListener{
+        binding.btnFollower.setOnClickListener {
 
             childFragmentManager.beginTransaction()
-                    .replace(R.id.container_view, followerFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-            binding.btnFollower.isSelected=true;
-            binding.btnRepo.isSelected=false;
+                .replace(R.id.container_view, followerFragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
+            binding.btnFollower.isSelected = true;
+            binding.btnRepo.isSelected = false;
         }
         binding.btnRepo.setOnClickListener {
             childFragmentManager.beginTransaction()
-                .replace(R.id.container_view,repositoryFragment)
+                .replace(R.id.container_view, repositoryFragment)
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .commit()
-            binding.btnRepo.isSelected=true
-            binding.btnFollower.isSelected=false
+            binding.btnRepo.isSelected = true
+            binding.btnFollower.isSelected = false
 
 
         }
@@ -67,66 +67,17 @@ class ProfileFragment : Fragment() {
             startActivity(intent)
         }
     }
-    private fun initImage(){
+
+    private fun initImage() {
         Glide.with(this)
             .load("https://avatars.githubusercontent.com/u/89780201?s=200&v=4")
             .circleCrop()
             .into(binding.imageView)
 
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-    }
-
-
-
-
-
-
-//class ProfileFragment : Fragment() {
-//    private var position=FOLLOWER_POSITION
-//    private lateinit var binding: ActivityHomeBinding
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding= ActivityHomeBinding.inflate(layoutInflater)
-//        initTransactionEvent()
-//        setContentView(binding.root)
-//    }
-//
-//    private fun initTransactionEvent(){
-//        val followerFragment = FollowerFragment()
-//        val repositoryFragment = RepositoryFragment()
-//
-//        supportFragmentManager.beginTransaction().add(R.id.container_view, followerFragment).commit()
-//
-//        binding.btnRepo.setOnClickListener {
-//            val transaction = supportFragmentManager.beginTransaction()
-//
-//            when (position){
-//                FOLLOWER_POSITION -> {
-//                    transaction.replace(R.id.container_view,repositoryFragment)
-//                    position = REPOSITORY_POSITION
-//                }
-//            }
-//            transaction.commit()
-//        }
-//
-//        binding.btnFollower.setOnClickListener{
-//            val transaction = supportFragmentManager.beginTransaction()
-//
-//            when (position){
-//                REPOSITORY_POSITION -> {
-//                    transaction.replace(R.id.container_view, followerFragment)
-//                    position = FOLLOWER_POSITION
-//                }
-//            }
-//            transaction.commit()
-//        }
-//    }
-//
-//    companion object {
-//        const val FOLLOWER_POSITION = 1
-//        const val REPOSITORY_POSITION = 2
-//    }
+}
